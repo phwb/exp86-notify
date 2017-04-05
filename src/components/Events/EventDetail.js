d1bdb416d304f55bd5ef13f471759deb8e6449ee
yvv <yvv@ugraweb.ru>
2017-04-05 12:06:12 +0500

    переделал Events и EventDetail на функциональные компоненты

--- src/components/Events/EventDetail.js
+++ src/components/Events/EventDetail.js
@@ -1,46 +1,35 @@
-import React, { Component, PropTypes } from 'react'
-// import { Providers } from '../Providers'
+import React, { PropTypes } from 'react'
+import { Providers } from '../Providers'
 
-export class EventDetail extends Component {
-  static propTypes = {
-    removeHandler: PropTypes.func.isRequired,
-    entityCode: PropTypes.string.isRequired,
-    event: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired
-    })
+export const EventDetail = props => {
+  const { event, removeHandler, entityCode } = props
+
+  if (!event) {
+    return null
   }
 
-  shouldComponentUpdate (nextProps) {
-    const { event } = this.props
-
-    if (!event) {
-      return true
-    }
-
-    return event.id !== nextProps.event.id
+  const handler = e => {
+    removeHandler(event.id)
+    e.preventDefault()
   }
 
-  render () {
-    const { event, removeHandler/*, entityCode */} = this.props
-
-    if (!event) {
-      return null
-    }
-
-    const handler = e => {
-      removeHandler(event.id)
-      e.preventDefault()
-    }
-
-    return (
-      <div className="event-detail">
-        <div className="event-detail__title">
-          { event.name } <a href="#" onClick={ handler }>Удалить</a>
-        </div>
-        {/*<hr/>
-        <Providers entityCode={ entityCode } eventId={ event.id } />*/}
+  return (
+    <div className="event-detail">
+      <div className="event-detail__title">
+        { event.name } <a href="#" onClick={ handler }>Удалить</a>
       </div>
-    )
-  }
+      <hr/>
+      <Providers entityCode={ entityCode } eventId={ event.id } />
+    </div>
+  )
+}
+
+
+EventDetail.propTypes = {
+  removeHandler: PropTypes.func.isRequired,
+  entityCode: PropTypes.string.isRequired,
+  event: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired
+  })
 }
--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,68 +1,53 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { EventSelect } from './EventSelect'
 import { EventList } from './EventList'
 import { EventDetail } from './EventDetail'
 import { add as addEvent, select as selectEvent, remove as removeEvent } from '../../actions/events'
 
+export const Events = props => {
+  const {
+    loading, items, entityCode, event,
+    addEvent, selectEvent, removeEvent
+  } = props
 
-export class Events extends Component {
-  static propTypes = {
-    entityCode: PropTypes.string,
-    loading: PropTypes.bool,
-    items: PropTypes.array,
-    event: PropTypes.object,
-    addEvent: PropTypes.func,
-    selectEvent: PropTypes.func,
-    removeEvent: PropTypes.func
+  if (loading) {
+    return <div>Loading events...</div>
   }
 
-  // remove (id) {
-  //   if (!id) {
-  //     return
-  //   }
-  //
-  //   getJSON(`/entities/${this.props.entityCode}/events/${id}`, {
-  //     method: 'DELETE'
-  //   })
-  //     .then(() => this.resetItems())
-  //     .catch(console.error)
-  // }
+  if (!items.length) {
+    return null
+  }
 
-  render () {
-    const {
-      loading, items, entityCode, event,
-      addEvent, selectEvent, removeEvent
-    } = this.props
+  // доступные для регистрации события
+  const available = items.filter(item => item.id === 0)
+  // уже зарегистрированные события
+  const registered = items.filter(item => item.id > 0)
 
-    if (loading) {
-      return <div>Loading events...</div>
-    }
-
-    if (!items.length) {
-      return null
-    }
-
-    // доступные для регистрации события
-    const available = items.filter(item => item.id === 0)
-    // уже зарегистрированные события
-    const registered = items.filter(item => item.id > 0)
-
-    return (
-      <div className="events">
-        <div className="event-list">
-          <EventSelect items={ available } onChange={ code => addEvent(entityCode, code) }/>
-          <EventList items={ registered } onClick={ id => selectEvent(id) }/>
-        </div>
-        <hr/>
-        <EventDetail
-          removeHandler={ id => removeEvent(entityCode, id) }
-          event={ event }
-          entityCode={ entityCode }
-        />
+  return (
+    <div className="events">
+      <div className="event-list">
+        <EventSelect items={ available } onChange={ code => addEvent(entityCode, code) }/>
+        <EventList items={ registered } onClick={ id => selectEvent(id) }/>
       </div>
-    )
-  }
+      <hr/>
+      <EventDetail
+        removeHandler={ id => removeEvent(entityCode, id) }
+        event={ event }
+        entityCode={ entityCode }
+      />
+    </div>
+  )
+}
+
+Events.propTypes = {
+  entityCode: PropTypes.string,
+  loading: PropTypes.bool,
+  items: PropTypes.array,
+  event: PropTypes.object,
+  addEvent: PropTypes.func,
+  selectEvent: PropTypes.func,
+  removeEvent: PropTypes.func
 }
 
 const mapStateToProps = state => {
@@ -86,4 +71,21 @@ const mapDispatchToProps = {
   removeEvent
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Events)
+const mergeProps = (stateProps, dispatchProps) => {
+  const { event } = stateProps
+  const { selectEvent } = dispatchProps
+
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    selectEvent: id => {
+      if (!event) {
+        return selectEvent(id)
+      }
+
+      event.id !== id && selectEvent(id)
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Events)
